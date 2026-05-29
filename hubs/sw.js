8fc25cd8a46aa097a14ec0be3d6c439890357920
centiger <centiger2@gmail.com>
2026-05-29 18:35:14 +0900

    Add files via upload

--- hubs/sw.js
+++ hubs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cen-return-matrix-v4-preservation-fixed';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});
+const CACHE='cen-return-exile-hubs-v4-law-hub';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./assets/maps/return-hub-map.png','./assets/maps/temple-hub-map.png','./assets/maps/preservation-hub-map.png','./assets/maps/law-hub-map.png'])))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))));});
